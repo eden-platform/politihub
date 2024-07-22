0d023f379557f8003fa66ee25126cbd9be586c95
Justin Donnaruma <justin@donnaruma.us>
2024-07-22 10:50:37 -0400

    Refactoring from Gameplan to PolitiHub

---
+++ politihub/politihub/doctype/gp_notification/gp_notification.py
@@ -0,0 +1,29 @@
+# Copyright (c) 2022, Frappe Technologies Pvt Ltd and contributors
+# For license information, please see license.txt
+
+import frappe
+import politihub
+from frappe.model.document import Document
+
+class GPNotification(Document):
+	def after_insert(self):
+		politihub.refetch_resource('Unread Notifications Count', user=self.to_user)
+
+	@staticmethod
+	def clear_notifications(discussion=None, comment=None, task=None, user=None):
+		if not user:
+			user = frappe.session.user
+		filters = {'to_user': user}
+		if discussion:
+			filters['discussion'] = discussion
+		if comment:
+			filters['comment'] = comment
+		if task:
+			filters['task'] = task
+
+		for notification in frappe.get_all('GP Notification', filters=filters):
+			doc = frappe.get_doc('GP Notification', notification.name)
+			doc.read = 1
+			doc.save()
+
+		politihub.refetch_resource('Unread Notifications Count', user=user)
